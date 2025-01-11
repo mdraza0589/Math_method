65c589bc8d27534aa8f522d74e48bfc7a5cf8ea0
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-01-11 14:29:40 +0530

    Add files via upload

---
+++ math_method.js
@@ -0,0 +1,38 @@
+// let number = 25;
+// let squareRoot = Math.sqrt(number);  // print the squareRoot of a number
+// console.log(squareRoot);
+
+
+// let number = 20.34;
+// let ans = Math.floor(number)  // change the decimal to integer
+// console.log(ans);
+
+
+// let number = 55.232;
+// let ans = Math.PI;  // PI provide value to pi(3.14)
+// console.log(ans);
+
+
+// let number = 33.22;
+// let ans = Math.ceil(number);   //Ceil change the decimal number to integer number
+// console.log(ans);
+
+
+// let number = 33.22;
+// let ans = Math.round(number) // decimal number change into integer number
+// console.log(ans);
+
+
+// let power = Math.pow(5,2);
+// console.log(power);
+
+
+// let number = -22.33;
+// let ans = Math.abs(number)  // abs change the negative value to positive value
+// console.log(ans);
+
+
+
+// let number = 33.22;
+// let ans = Math.trunc(number)   // trunc change the decimal number to integer number
+// console.log(ans);
